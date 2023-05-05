894766fe91a7f30c510940177818698d2ebbb910
Gaurav Jaiswal <gauravjaiswal.mail@gmail.com>
2023-05-05 08:20:33 +0100

    Implemented calculateTotalCost in ShoppingCart

--- src/main/scala/com/cart/ShoppingCart.scala
+++ src/main/scala/com/cart/ShoppingCart.scala
@@ -3,7 +3,10 @@ package com.cart
 object ShoppingCart {
 
   def calculateTotalCost(items: List[String]): Double = {
-    val totalCost = 0.0
+    val apple = 0.60
+    val orange = 0.25
+
+    val totalCost = apple * items.count(_ == "Apple") + orange * items.count(_ == "Orange")
 
     totalCost
   }
