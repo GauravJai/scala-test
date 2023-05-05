b71d918de0dc19a11c9cf9e6b368d4532f198ec7
Gaurav Jaiswal <gauravjaiswal.mail@gmail.com>
2023-05-05 09:04:39 +0100

    Step 2: Simple offers :: Implemented new discount features buyOneGetOneApple, threeForTwoOrange and integrated discount to calculateTotalCost

--- src/main/scala/com/cart/ShoppingCart.scala
+++ src/main/scala/com/cart/ShoppingCart.scala
@@ -5,15 +5,16 @@ object ShoppingCart {
   def calculateTotalCost(items: List[String]): Double = {
     val apple = 0.60
     val orange = 0.25
-    apple * items.count(_ == "Apple") + orange * items.count(_ == "Orange")
+    apple * buyOneGetOneApple(items.count(_ == "Apple"))
+      + orange * threeForTwoOrange(items.count(_ == "Orange"))
   }
 
   def buyOneGetOneApple(apples: Int): Int = {
-      0
+    apples / 2 + apples % 2
   }
 
   def threeForTwoOrange(oranges: Int): Int = {
-    0
+    oranges - (oranges / 3)
   }
 
 }
